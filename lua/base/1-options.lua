beafa8586e836d0010428785a3974d1a87ea27b6
Javed Khan <tuxcanfly@gmail.com>
2024-03-27 18:57:33 -0700

    opt: autoformat

--- lua/base/1-options.lua
+++ lua/base/1-options.lua
@@ -75,7 +75,7 @@ vim.g.maplocalleader = "," -- Set default local leader key.
 vim.g.big_file = { size = 1024 * 100, lines = 10000 } -- For files bigger than this, disable 'treesitter' (+100kb).
 
 -- The next globals are toggleable with <space + l + u>
-vim.g.autoformat_enabled = false -- Enable auto formatting at start.
+vim.g.autoformat_enabled = true -- Enable auto formatting at start.
 vim.g.autopairs_enabled = false -- Enable autopairs at start.
 vim.g.cmp_enabled = true -- Enable completion at start.
 vim.g.codelens_enabled = true -- Enable automatic codelens refreshing for lsp that support it.
